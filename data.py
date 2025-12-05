cf68cfdb88144961c0a481e70d61dc9e13b120ce
Anastasiia Mironova <m1rona.a@yandex.ru>
2025-12-05 22:01:06 +0300

    add test data

---
+++ data.py
@@ -0,0 +1,29 @@
+class Urls:
+    link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+class Language:
+    langs = [
+        "ar",
+        "ca",
+        "cs",
+        "da",
+        "de",
+        "en-gb",
+        "el",
+        "es",
+        "fi",
+        "fr",
+        "it",
+        "ko",
+        "nl",
+        "pl",
+        "pt",
+        "pt-br",
+        "ro",
+        "ru",
+        "sk",
+        "uk",
+        "zh-hans",
+    ]
+
