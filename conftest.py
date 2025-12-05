d9fea7d4ec8932950ec451e4bb18da24fe7ef7df
Anastasiia Mironova <m1rona.a@yandex.ru>
2025-12-05 22:02:54 +0300

    add fixture and custom option

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import pytest
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from data import Language as L, Urls as U
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="ru",
+                     help=f"Set default language: {L.langs}")
+
+
+@pytest.fixture
+def browser(request):
+    user_language = request.config.getoption("language")
+    browser = None
+    options = Options()
+    if user_language not in L.langs:
+        raise pytest.UsageError(f"Invalid language. Choose default language from: {L.langs}")
+    options.add_experimental_option("prefs", {"intl.accept_languages": user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    browser.get(U.link)
+    time.sleep(30)
+    browser.quit()
