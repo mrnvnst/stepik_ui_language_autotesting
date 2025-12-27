94c6228ac8dce1d6ecd4ee2db42fda37ec3e1afd
Anastasiia Mironova <m1rona.a@yandex.ru>
2025-12-27 21:21:20 +0300

    remove non-existent bottom button locator

--- locators.py
+++ locators.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-class GoodPageLocators:
-    BASKET_UPPER_BTN = (By.CSS_SELECTOR, "button.btn-add-to-basket")
-    BASKET_BOTTOM_BTN = (By.CSS_SELECTOR, "button.btn-primary.btn-block")
+class GoodsPageLocators:
+    BASKET_BTN = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+
